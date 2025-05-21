76f2a45d5a26f9dc7539c1d48a914ec46a774f67
Pramoda-S-R <pramoda9.2.2004@gmail.com>
2025-05-21 11:12:00 +0530

    cors dealt with

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import pandas as pd
@@ -12,6 +13,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Load model and feature names
 model_path = "trained_models/Random_Forest.pkl"
